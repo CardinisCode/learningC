df101025e0abfa28e7cbab1bf56b84462ff915b9
Andi Folgado <ac.folgado@gmail.com>
2020-08-06 16:23:46 +0100

    Tideman challenge: Solved!
    I have successfully written and completed all 6 functions for the Tideman challenge. I have also written up the tideman.md to state the details of the challenge including a 'table of contents' listing the different sections included within with their perspective line numbers.

--- tideman.c
+++ tideman.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// All the below is their code defining the various variables and structs that will be needed.
+
 // Max number of candidates
 #define MAX 9
 
@@ -31,6 +33,7 @@ bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
 void add_pairs(void);
 void sort_pairs(void);
+bool is_cycle(pair past, int loser, int pair_index);
 void lock_pairs(void);
 void print_winner(void);
 
@@ -81,8 +84,7 @@ int main(int argc, string argv[])
 
             if (!vote(j, name, ranks))
             {
-                printf("Your vote function didnt work as expected\n");
-                //printf("Invalid vote.\n");
+                printf("Invalid vote.\n");
                 return 3;
             }
         }
@@ -92,10 +94,12 @@ int main(int argc, string argv[])
 
     add_pairs();
     sort_pairs();
-    // lock_pairs();
-    // print_winner();
+    lock_pairs();
+    print_winner();
     return 0;
 }
+
+// The below functions are for me to write to ensure the main() function works as expected. 
 
 // Update ranks given a new vote
 // ranks[i] is voter's ith preference so:
@@ -103,36 +107,53 @@ int main(int argc, string argv[])
 //  -   ranks[1] = voter's 2nd preference
 //  -   ranks[2] = voter's 3rd preference
 
-// Vote works!
+// This function makes sure that the voter's current candidate (in iteration) is in fact in the election.
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    // We'll iterate over the candidates to see if there's any candidate that matches the name provided by the current voter:
     for (int candidate = 0; candidate < candidate_count; candidate++)
     {
+        // We'll grab the name of the current candidate (the candidate sitting at index 'candidate' in the candidates array)
         string current_candidate = candidates[candidate];
+        // If the name of the current candidate matches the 'name' (provided by the voter)
         if (strcmp(current_candidate, name) == 0)
         {
+            // Then we'll update the ranks array to reflect that at this 'rank', we'll find the index 'candidate' for the current candidate
             ranks[rank] = candidate;
+            // Now that we've found a match, this function can true & exit. The function has served it's purpose. 
             return true;
         }
     }
+    // If it gets to the point, it means no matches were found 
+    //  -> this means voter voted for a candidate not in the election.
     return false;
 }
 
-// Update preferences given one voter's ranks
+// Update preferences for current voter given the voter's candidates in ranked order
 void record_preferences(int ranks[])
 {
-    // TODO
+    // We'll first iterate over the candidate array from the beginning to the end-1
+    // Eg: 0, 1, 2, 3 (not including 4)
     for (int preferred = 0; preferred < candidate_count - 1; preferred++)
     {
+        // It will find the candidate found at the 'preferred' index in the ranks array and this candidate will be our preferred candidate
+        // It will start from the most preferred candidate and descend down to the least preferred candidate
         int prefererred_candidate = ranks[preferred];
+
+        // Now to look over our candidates array from the current preferred candidate's index + 1 to the end of the candidate array
+        // Eg 1, 2, 3, 4
+        // So the candidate at index 0 is compared to the candidate at index 1 & so forth. 
         for (int over = preferred + 1; over < candidate_count; over++)
         {
+            // It will find the candidate found at the 'over' index in our ranks array and this candidate will be our over_candidate
             int over_candidate = ranks[over];
+
+            // Now we update our preference array to reflect that the 'preferred' candidate has +1 vote against the 'over' candidate
             preferences[prefererred_candidate][over_candidate] += 1;
         }
     }
-
+    // This is to allow me see the output of the preferences array in printed form
+    // to follow the progress as each voter's preferences are added in.
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
@@ -147,73 +168,154 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    // We'll compare the candidates against each other
     for (int candidate_i = 0 ; candidate_i < candidate_count; candidate_i++)
     {
         for (int candidate_j = 0; candidate_j < candidate_count; candidate_j++)
         {
+            // Then we check to see in the current pair, which candidate has the higher votes (our winner)
             if (preferences[candidate_i][candidate_j] > preferences[candidate_j][candidate_i])
             {
+                // We can now update the current pair's winner (who has the higher votes) and loser
                 pairs[pair_count].winner = candidate_i;
                 pairs[pair_count].loser = candidate_j;
+                // And update the pair count to reflect that we've found a valid pair.
                 pair_count += 1;
             }
+            // If the 2 candidates in the pair are in a draw, we don't do anything and move onto the next pair
+            // If preferences[candidate_j][candidate_i] > preferences[candidate_i][candidate_j], 
+            //  it will be picked up when candidate i's index reaches that index value. 
         }
     }
+    // Now our pairs have been added to the pairs array with each pair having their allocated winner and loser.
     return;
 }
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
+    // For every (current) pair:
     for (int current = 0; current < pair_count - 1; current++)
     {
+        // We'll get the winner, loser and calculate the strength of victory for this current pair:
         int current_winner = preferences[pairs[current].winner][pairs[current].loser];
         int current_loser = preferences[pairs[current].loser][pairs[current].winner];
         int current_strength_of_victory = current_winner - current_loser; 
 
+        // We want to create a winning index variable whose's value will default to the current index in iteration. 
         int winning_index = current; 
 
+        // Now to compare the current pair to every other pair in the pairs array:
         for (int comparison = current + 1; comparison < pair_count; comparison++)
         {
+            // We'll get the comparison pair's winner, loser & calculate the comparison's strength of victory
             int comparison_winner = preferences[pairs[comparison].winner][pairs[comparison].loser];
             int comparison_loser = preferences[pairs[comparison].loser][pairs[comparison].winner];
             int comparison_strength_of_victory = comparison_winner - comparison_loser;
 
+            // If any of the comparison pairs have a higher strength of victory compared to the current's strength of victory
             if (comparison_strength_of_victory > current_strength_of_victory)
             {
+                // Then we want to update the winning index to reflect the index where that comparison pair was found
+                // in the pair array:
                 winning_index = comparison; 
             }
         }
-
+        
+        // For every current pair, we'll need to switch the pairs[current] with the pair[winning_index] 
         pair temp = pairs[current];
         pairs[current] = pairs[winning_index]; 
         pairs[winning_index] = temp;
     }
-
-    // for (int pair = 0; pair < candidate_count; pair++)
-    // {
-    //     int winner_votes = preferences[pairs[pair].winner][pairs[pair].loser];
-    //     int loser_votes = preferences[pairs[pair].loser][pairs[pair].winner];
-    //     int difference = winner_votes - loser_votes;
-
-    //     printf("At Index %i: Winner %i with %i votes & Loser: %i with %i votes\n & a difference: %i\n", pair, pairs[pair].winner, winner_votes, pairs[pair].loser, loser_votes, difference);
-    //     // printf("At index %i: winner: %i loser: %i %i\n", pair, pairs[pair].winner, pairs[pair].loser);
-    // }
     return;
 }
 
-// // Lock pairs into the candidate graph in order, without creating cycles
-// void lock_pairs(void)
-// {
-//     // TODO
-//     return;
-// }
+// This function is designed to be called by lock_pairs()
+//  It will take the current loser from lock_pairs and iterate through all the pairs 
+//  from the past pair index (from lock_pairs) -1 -> the pair at index 0
+//  And if at any point, the current loser matches any of the pairs' winner's, then it has found a cycle
+// So it will return true. 
 
-// // Print the winner of the election
-// void print_winner(void)
-// {
-//     // TODO
-//     return;
-// }
+bool is_cycle(pair past, int loser, int pair_index)
+{
+    // Let's iterate over the pairs in reverse order from pair_index-1 back to 0. 
+    for (int i = pair_index - 1; i >= 0; i--)
+    {
+        pair current = pairs[i]; 
+        if (past.winner == current.loser)
+        {
+            // There's a link
+            printf("There's a link: %i\n", past.winner);
+            if (locked[current.winner][current.loser] == true && loser == current.winner)
+            {
+                // There's a cycle!
+                return true;
+            }
+            // else -> call the function recursively with the current pair, the original loser and the current index
+            return is_cycle(current, loser, i);
+        }
+    }
+    // No cycle found by this point so it returns false and exits.
+    return false;
+}
+
+// Lock pairs into the candidate graph in order, without creating cycles
+void lock_pairs(void)
+{
+    // It will iterate over every pair in the pairs array:
+    for (int pair_index = 0; pair_index < pair_count; pair_index++)
+    {
+        // It will grab the winner and loser from that pair
+        int winner = pairs[pair_index].winner; 
+        int loser = pairs[pair_index].loser;
+
+        // Then it will run is_cycle with the pair 'pairs[pair_index]', the current loser, & current pair index
+        // If is_cycle resolves to false:
+        if (!is_cycle(pairs[pair_index], loser, pair_index))
+        {
+            // Then we can update the locked array so there's now an arrow pointing from the winner to the loser
+            locked[winner][loser] = true;
+        }
+        // If the is_cycle resolves to true, we do nothing and move onto the next pair.
+    }
+    return;
+}
+
+
+// // Print the winner of the election (the source of the graph)
+void print_winner(void)
+{
+    // For every candidate:
+    for (int current = 0; current < candidate_count; current++)
+    {
+        // We'll set the candidateIsSource to a defaul false value
+        bool candidateIsSource = false;
+
+        // Before comparing current candidate to every other candidate in the array:
+        for (int comparison = 0; comparison < candidate_count; comparison++)
+        {
+            // If candidate[comparison] is pointing at candidate[current]:
+            if (locked[comparison][current] == true)
+            {
+                // Then candidate[current] is not the source.  
+                // Update candidate's candidateIsSource to true to reflect they have an arrow pointing to them.
+                candidateIsSource = true;
+                // Move onto the next candidate.
+                break;
+            }
+        }
+        // After we've compared the current candidate to all their peers
+        //  we can check if their candidateIsSource still remains as false 
+        //  -> to indicate no arrows were found to be pointing to the current candidate
+
+        if (candidateIsSource == false)
+        {
+            // We can print the candidate's name and break to make sure it doesn't move onto the next candidate
+            // in our outer loop (current)
+            printf("%s\n", candidates[current]);
+            break;
+        }
+    }
+    return;
+}
 
--- tideman.md
+++ tideman.md
@@ -1,5 +1,28 @@
 The Tideman challenge: 
 
+The main() function asks the user to provide the candidates each voter elected in ranked order. 
+It's our job to write code that finds the winner of the election.
+
+*** Desired outcome: 
+- The code we write 
+    -   for functions: 
+        -   vote
+        -   record_preferences
+        -   add_pairs
+        -   sort_pairs
+        -   lock_pairs
+        -   print_winner
+
+    Should enable their main() function to find the source candidate (the winner) in the election 
+    -   using the tideman algorithm/process 
+    -   & print the winner's name at the end. 
+
+Below you'll find:          Line:
+-   Challenge background    26
+-   Code Breakdown          50
+-   Our challenge           111
+
+
 ***Background:
 
 Put more formally, the Tideman voting method consists of three parts:
@@ -24,9 +47,7 @@ Put more formally, the Tideman voting method consists of three parts:
 
 Once the graph is complete, the source of the graph (the one with no edges pointing towards it) is the winner!
 
-***Understanding:
-
-Let’s open up tideman.c to take a look at what’s already there.
+Code Breakdown:
 
 #1: There are 2x two-dimensional arrays 
     *1: preferences. 
@@ -57,7 +78,7 @@ Let’s open up tideman.c to take a look at what’s already there.
 -   candidate_count
 ->  representing the number of pairs and number of candidates in the arrays pairs and candidates, respectively.
 
-#3: Now onto main. 
+#3: Now onto main(). 
 -   *1: It determines the number of candidates
 
 -   *2: It loops through the locked graph &
@@ -87,20 +108,26 @@ Finally:
 - *8:   print the name of the election’s winner
         ->  via a call to print_winner
 
-Further down in the file, you’ll see that the functions vote, record_preference, add_pairs,sort_pairs, lock_pairs, and print_winner are left blank. That’s up to you!
+** Our challenge:
 
-The functions: 
+Further down in the file, you’ll see that there are 6 functions left blank:
+-   vote, record_preference, add_pairs,sort_pairs, lock_pairs, and print_winner
+->  That’s up to you!
+
 * 1: vote()
 -   The function takes arguments
     -   rank
     -   name
     -   ranks. 
 
--   If name is a match for the name of a valid candidate, 
+-   If name is a match for the name of a valid candidate (ie a candidate actually in the election), 
     -   then you should update the ranks array
-        -   to indicate that the voter has the candidate
+        -   to indicate that the voter has that candidate
             as their rank preference 
             (where 0 is the first preference, 1 is the second preference, etc.)
+    so if voter 0 votes A, B, C
+    then the ranks will be: 
+        0: A  1: B  2: C
 
 -   ranks[i] here represents the user’s ith preference.
 
@@ -110,4 +137,82 @@ The functions:
     (if, for instance, name is not the name of one of the candidates)
     -   return false  
 -   You may assume that no two candidates will have the same name.
+
+* 2: record_preferences
+-   The function is called once for each voter, 
+-   takes (as argument) the ranks array, 
+    (recall that ranks[i] is the voter’s ith preference, where ranks[0] is the first preference).
+-   The function should update the global preferences array
+    -   to add the current voter’s preferences. 
+        (Recall that preferences[i][j] should represent the number of voters who prefer candidate i over candidate j.)
+-   You may assume that every voter will rank each of the candidates.
+
+- So if voter 0 votes A, B, C
+->  Then voter 0 
+    -   prefers A over B & C
+        -   preferences[A][B] +=1
+        -   preferences[A][C] +=1
+    -   prefers B over C
+        -   preferences[B][C] +=1
+
+* 3: add_pairs
+-   The function should add a pair (to the pairs array) if
+    -   one candidate is preferred over another 
+    -   both candidates are not in a tie (ie both candidates have the same number of overall votes)
+-   It should then update the global variable pair count to reflect the number of pairs that have been added to the pairs array.
+    -   The pairs should thus all be stored between pairs[0] and pairs[pair_count - 1], inclusive.
+
+-   So in pair A & B, if A has 5 votes and B has 3 votes, 
+    it would be a valid pair 
+    -   It gets added to the pair array
+    -   We add +1 to the pairs count 
+
+    But if A & B both have 4 votes, 
+    -   We don't add this pair to the pairs array
+
+* 4: sort_pairs
+-   The function should sort the pairs array in decreasing order of strength of victory, 
+    -   where strength of victory is defined to be the number of voters who prefer the preferred candidate. 
+    -   If multiple pairs have the same strength of victory, you may assume that the order does not matter.
+Hint: Consider the difference in the votes between the winner and the loser of every pair and sort according to this difference
+- We basically want to sort the pairs so they're in order 
+    from:   the strongest victory
+        (the pair with the greatest difference between the winner and the loser)
+    To:     the weakest victory
+        (the pair with the least difference between the winner and the loser)
+    -> Where pairs with the same difference can be in any order between themselves
+
+So we have the following pairs                      Sorted version:
+1)  B = 5 votes C = 4 votes -> Difference = 1       A = 7votes B = 2 votes -> difference = 5             
+2)  A = 3 votes C = 6 Votes -> Difference = 3       A = 3 votes C = 6 Votes -> Difference = 3 
+3)  A = 7votes B = 2 votes -> difference = 5        B = 5 votes C = 4 votes -> Difference = 1 
+ 
+* 5: lock_pairs 
+-   The function should create the locked graph, 
+    -   adding all edges in decreasing order of victory strength so long as the edge would not create a cycle.
+
+-   Here's where you're actually adding the edges/arrows from winner to loser, 
+    -   whilst ensuring that adding the arrow doesn't create a cycle 
+
+Using the previous stats, in sorted order from strongest to weakest:
+    0: A - B
+    1: C - A
+    2: B - C
+
+If we added all these arrows, we'd definitely get a cycle (C -> A -> B -> C)
+So we're told to ignore any pair that would create this cycle: 
+    C -> A -> B
+Now C doesn't have any arrows pointing to it! 
+
+** Note this may work out to be the hardest function in this challenge. Don't give up! 
+-> Feel free to create more than 1 function to solve this problem
+
+* 6: print_winner
+-   The function should print out the name of the candidate who is the source of the graph. 
+    -> Basically the only candidate that has no arrows pointing to it.
+-   You may assume there will not be more than one source.
+
+
+    
+
 
